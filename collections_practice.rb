d46d2dff444b19e76f516c1e69e2b76e5c0d5fd6
Saad Malik <saad.t.malik@gmail.com>
2018-11-18 11:12:07 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,13 +9,13 @@ def sort_array_desc (integers)
 end
 
 def sort_array_char_count(strings)
-  strings.sort do |left, right| 
+  strings.sort do |left, right|
     left.length <=> right.length
   end
 end
 
 def swap_elements(strings)
-  strings[1], strings[2] = strings[2], strings[1]
+  strings[2], strings[3] = strings[3], strings[2]
   return strings
 end
 
